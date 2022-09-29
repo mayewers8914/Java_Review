dbc0d839b141689df5f0480e16085c8857cb6c79
mestipule <mestipul@gmail.com>
2022-09-29 11:26:29 -0400

    Collection Review

--- collections/src/main/java/ArrayListReview.java
+++ collections/src/main/java/ArrayListReview.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class ArrayListReview {
 
@@ -24,37 +23,79 @@ public class ArrayListReview {
 
         */
 
-        List<String> arrayTest = new ArrayList();
+        List<Student> student = new ArrayList();
 
-        arrayTest.add("May");
-        arrayTest.add("John");
-        arrayTest.add("Phoebe");
-        arrayTest.add("Zoe");
+        student.add(new Student("May", 1));
+        student.add(new Student("John", 2));
+        student.add(new Student("Phoebe", 3));
+        student.add(new Student("Zoe", 4));
 
-        System.out.println(arrayTest.size());
-        System.out.println(arrayTest.get(3));
-        System.out.println(arrayTest.contains("Zoe"));
-        System.out.println(arrayTest.indexOf(2));
+        System.out.println(student.size());
+        System.out.println(student.get(3));
+        System.out.println(student.indexOf(2));
 
-        for (int i = 0; i < arrayTest.size(); i++) {
-            System.out.println(arrayTest.get(i));
+        for (int i = 0; i < student.size(); i++) {
+            System.out.println(student.get(i));
         }
 
-        for (String name : arrayTest) {
+        for (Student name : student) {
             System.out.println(name);
         }
 
-        arrayTest.forEach(
-                (name)-> System.out.println(name)
+        //lambda
+        System.out.println("lambda");
+        student.forEach(
+                studentInfo-> System.out.println(student)
         );
 
         System.out.println("Printing backwards");
 
-        for (int i = arrayTest.size() - 1; i >=0; i--) {
-            System.out.println(arrayTest.get(i));
+        for (int i = student.size() - 1; i >=0; i--) {
+            System.out.println(student.get(i));
         }
 
+        int num = 1;
 
+        //iterator
+        Iterator iter = student.listIterator();
+        while (iter.hasNext()) {
+            System.out.println("Iterator " + num);
+            System.out.println(iter.next());
+            num++;
+        }
+
+        System.out.println("Print Backwards");
+
+        while (((ListIterator) iter).hasPrevious()) {
+            System.out.println(((ListIterator) iter).previous());
+        }
+
+        //sorting
+        System.out.println("sorting");
+
+        System.out.println("using comparator to sort");
+        Collections.sort(student, new sortById());
+        System.out.println(student);
+
+        Collections.sort(student, new sortByName());
+        System.out.println(student);
+
+    }
+
+    static class sortById implements Comparator<Student> {
+
+        @Override
+        public int compare(Student o1, Student o2) {
+            return o2.id-o1.id;
+        }
+    }
+
+    static class sortByName implements Comparator<Student> {
+
+        @Override
+        public int compare(Student o1, Student o2) {
+            return o1.name.compareToIgnoreCase(o2.name);
+        }
     }
 
 }
